2fa783338eb03e8b8c0d89a1124dfee9057fee7b
brianwambuamu <mutisobrian210@gmail.com>
2023-03-29 22:58:15 +0300

    working on migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,83 +6,83 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "🌱 seeding"
+# puts "🌱 seeding"
 
-puts "users..."
-users = []
+# puts "users..."
+# users = []
 
-# Main Users
-users << User.create(
-        full_name: "Brian Wambua",
-        email: "brian@example.com",
-        username: "brianm",
-        password: "brian",
-        password_confirmation: "brian",
-        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
-        profile_picture: Faker::Avatar.image
-    )
+# # Main Users
+# users << User.create(
+#         full_name: "Brian Wambua",
+#         email: "brian@example.com",
+#         username: "brianm",
+#         password: "brian",
+#         password_confirmation: "brian",
+#         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+#         profile_picture: Faker::Avatar.image
+#     )
 
-users << User.create(
-        full_name: "Lesayo Bruce",
-        email: "bruce@example.com",
-        username: "brucel",
-        password: "bruce",
-        password_confirmation: "bruce",
-        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
-        profile_picture: Faker::Avatar.image
-    )
+# users << User.create(
+#         full_name: "Lesayo Bruce",
+#         email: "bruce@example.com",
+#         username: "brucel",
+#         password: "bruce",
+#         password_confirmation: "bruce",
+#         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+#         profile_picture: Faker::Avatar.image
+#     )
 
-users << User.create(
-        full_name: "Bruno Kiptoo",
-        email: "bruno@example.com",
-        username: "brunok",
-        password: "bruno",
-        password_confirmation: "brno",
-        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
-        profile_picture: Faker::Avatar.image
-    )
+# users << User.create(
+#         full_name: "Bruno Kiptoo",
+#         email: "bruno@example.com",
+#         username: "brunok",
+#         password: "bruno",
+#         password_confirmation: "brno",
+#         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+#         profile_picture: Faker::Avatar.image
+#     )
 
-users << User.create(
-        full_name: "Brian Nderitu",
-        email: "nderitu@example.com",
-        username: "nderitub",
-        password: "nderitu",
-        password_confirmation: "nderitu",
-        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
-        profile_picture: Faker::Avatar.image
-    )
+# users << User.create(
+#         full_name: "Brian Nderitu",
+#         email: "nderitu@example.com",
+#         username: "nderitub",
+#         password: "nderitu",
+#         password_confirmation: "nderitu",
+#         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+#         profile_picture: Faker::Avatar.image
+#     )
 
-users << User.create(
-        full_name: "Andrew Ochieng",
-        email: "andrew@example.com",
-        username: "andrew",
-        password: "andrew",
-        password_confirmation: "andrew",
-        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
-        profile_picture: Faker::Avatar.image
-    )
+# users << User.create(
+#         full_name: "Andrew Ochieng",
+#         email: "andrew@example.com",
+#         username: "andrew",
+#         password: "andrew",
+#         password_confirmation: "andrew",
+#         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+#         profile_picture: Faker::Avatar.image
+#     )
 
-# More Users
-11.times do |i|
-    password  = Faker::Internet.password
+# # More Users
+# 11.times do |i|
+#     password  = Faker::Internet.password
 
-    users << User.create(
-        full_name: Faker::Name.name,
-        email: Faker::Internet.email,
-        username: Faker::Name.name.split(" ").join("")+i.to_s,
-        password: password,
-        password_confirmation: password,
-        bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
-        profile_picture: Faker::Avatar.image
-    )
-end
+#     users << User.create(
+#         full_name: Faker::Name.name,
+#         email: Faker::Internet.email,
+#         username: Faker::Name.name.split(" ").join("")+i.to_s,
+#         password: password,
+#         password_confirmation: password,
+#         bio: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+#         profile_picture: Faker::Avatar.image
+#     )
+# end
 
-puts "messages..."
-messages = []
-500.times do
-    messages << Message.create(
-        content: Faker::Lorem.paragraph(sentence_count: rand(1..2)),
-        sender: users.sample.id,
-        receiver: users.sample.id
-    )
-end
+# puts "messages..."
+# messages = []
+# 500.times do
+#     messages << Message.create(
+#         content: Faker::Lorem.paragraph(sentence_count: rand(1..2)),
+#         sender: users.sample.id,
+#         receiver: users.sample.id
+#     )
+# end
